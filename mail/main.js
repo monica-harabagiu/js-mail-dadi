ba800b7b1c7583b786121fd35de323bedf21573c
monica-harabagiu <monica.harabagiu.design@gmail.com>
2024-01-10 16:20:26 +0100

    Email function

--- mail/main.js
+++ mail/main.js
@@ -4,23 +4,52 @@
 
 const email = ["correct@email.com", "your.email@email.com", "you.can.enter@email.com", "right@email.com"]
 
-for (let i = 0; i < email.length; i++) {
-    console.log(email[i])
-}
+// for (let i = 0; i < email.length; i++) {
+//     console.log(email[i])
+// }
 
-let variabileControllo = false;
+// let variabileControllo = false;
 
-for (let i = 0; i < email.length; i++) {
+// for (let i = 0; i < email.length; i++) {
 
-    if (email[i] = email) {
-        variabileControllo = true
-    }
-}
+//     if (email[i] === "correct@email.com") {
+//         variabileControllo = true
+//     }
+// }
 
-if (variabileControllo == true) {
-    console.log("La mail è corretta")
-} else {
-    console.log("La mail è sbagliata")
-}
+// if (variabileControllo == true) {
+//     console.log("La mail è corretta")
+// } else {
+//     console.log("La mail è sbagliata")
+// }
+
 
 let emailHtml = document.getElementById("email")
+const resultHtml = document.getElementById("result")
+const buttonHtml = document.getElementById("login-button")
+
+
+function verificaEmail() {
+
+    let variabileControllo = false;
+
+    for (let i = 0; i < email.length; i++) {
+        if (email[i] === emailHtml.value) {
+            variabileControllo = true;
+        }
+    }
+
+    if (variabileControllo) {
+        resultHtml.innerHTML = "La mail è corretta"
+        emailHtml.classList.remove("input-red")
+        emailHtml.classList.add("input-green")
+    } else {
+        resultHtml.innerHTML = "La mail è sbagliata"
+        emailHtml.classList.remove("input-green")
+        emailHtml.classList.add("input-red")
+    }
+
+}
+
+
+buttonHtml.addEventListener("click", verificaEmail)
